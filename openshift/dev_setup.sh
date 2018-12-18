745874dffdffbbeab60872fa4533ce75339ceae6
Justin Davis <justinndavis@gmail.com>
2018-12-18 18:02:20 +0000

    binary build

--- openshift/dev_setup.sh
+++ openshift/dev_setup.sh
@@ -2,19 +2,24 @@
 
 APP=inventory
 VERSION=dev
-S2I_IMAGE=redhat-openjdk18-openshift:1.5
+IMAGE=registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift:1.5
 IP=ocp.datr.eu
 USER=justin
 PROJECT=amazin-gitlab
 
-. ../../env.sh
-
 oc login https://${IP}:8443 -u $USER
+
+oc delete project $PROJECT
+oc adm new-project $PROJECT --node-selector='capability=apps' 2> /dev/null
+while [ $? \> 0 ]; do
+    sleep 1
+    printf "."
+oc adm new-project $PROJECT --node-selector='capability=apps' 2> /dev/null
+done
 
 oc project ${PROJECT}
 
 oc delete all -l app=${APP} -n ${PROJECT}
-oc delete pvc -l app=${APP} -n ${PROJECT}
 oc delete sa ${APP}-sa
 oc delete is,bc,dc,svc,route,sa ${APP} -n ${PROJECT}
 oc delete template ${APP}-dev-dc ${APP}-dev-template -n ${PROJECT}
@@ -23,7 +28,6 @@ oc delete configmap ${APP}-config -n ${PROJECT}
 echo Setting up ${APP} for ${PROJECT}
 oc new-app -f spring-boot-dev-template.yaml \
     -p APPLICATION_NAME=${APP} \
-    -p BASE_IMAGE_NAMESPACE="openshift" \
-    -p BASE_IMAGE=${S2I_IMAGE} \
+    -p BASE_IMAGE=${IMAGE} \
     -n ${PROJECT}
 
